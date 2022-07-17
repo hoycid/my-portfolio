dac5807555a5fe65e21fa31f132564fb9e9356e6
hoycid <quilangcidrex@gmail.com>
2022-07-17 16:12:22 +0800

    Use downscaled images for mobile and desktop

--- src/resources/imageList.js
+++ src/resources/imageList.js
@@ -1,5 +1,5 @@
-import foodorder from "../assets/images/Screenshot-2022-07-13-095101.webp";
-import portfolio from "../assets/images/Screenshot-2022-07-13-135147.webp";
+import foodorder from "../assets/images/food-app.webp";
+import portfolio from "../assets/images/portfolio.webp";
 
 const images = {
   foodorder: foodorder,
