de876d4d53a338881ffbe60913298d21271a7eb5
hoycid <quilangcidrex@gmail.com>
2023-09-28 18:54:42 +1300

    updated pagro-admin link

--- src/resources/links.js
+++ src/resources/links.js
@@ -1,7 +1,7 @@
 const links = {
   // foodorder: "https://react-foods-order.herokuapp.com/",
   // portfolio: "https://cidrexquilang.vercel.app/",
-  pagro: "https://pagro-admin-6lfavupyu-hoycid.vercel.app/",
+  pagro: "https://pagro-admin.vercel.app/",
 };
 
 export default links;
