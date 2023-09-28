7f5380c93eb6ec2638790d5ce024b3a7771e34af
hoycid <quilangcidrex@gmail.com>
2023-09-28 22:03:12 +1300

    hid portfolio and food order app in projects

--- src/resources/PORTFOLIO_LIST.js
+++ src/resources/PORTFOLIO_LIST.js
@@ -10,21 +10,21 @@ const PORTFOLIO_LIST = [
     technology: "[Javascript, NextJS, NodeJS]",
     link: "",
   },
-  {
-    title: "Portfolio website",
-    image: images.portfolio,
-    description:
-      "My personal portolio website to showcase my work history and recent projects.",
-    technology: "[Javascript, React, NodeJS]",
-    link: links.portfolio,
-  },
-  {
-    title: "Food Order App",
-    image: images.foodorder,
-    description: "A basic food order app with functional cart and menu.",
-    technology: "[Javascript, React, NodeJS]",
-    link: links.foodorder,
-  },
+  // {
+  //   title: "Portfolio website",
+  //   image: images.portfolio,
+  //   description:
+  //     "My personal portolio website to showcase my work history and recent projects.",
+  //   technology: "[Javascript, React, NodeJS]",
+  //   link: links.portfolio,
+  // },
+  // {
+  //   title: "Food Order App",
+  //   image: images.foodorder,
+  //   description: "A basic food order app with functional cart and menu.",
+  //   technology: "[Javascript, React, NodeJS]",
+  //   link: links.foodorder,
+  // },
 ];
 
 export default PORTFOLIO_LIST;
