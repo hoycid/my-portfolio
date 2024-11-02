f5c9b49563de3e2073ce61291b74e3c9263c8d72
hoycid <quilangcidrex@gmail.com>
2024-11-02 17:11:23 +1300

    changed links to target netlify deployments

--- src/resources/links.js
+++ src/resources/links.js
@@ -2,14 +2,14 @@ const links = {
   // foodorder: "https://react-foods-order.herokuapp.com/",
   portfolio: "https://cidrexquilang.vercel.app/",
   portfolioGithub: "https://github.com/hoycid/my-portfolio",
-  pagro: "https://pagro-admin.vercel.app/",
+  pagro: "https://pagro-admin.netlify.app/",
   pagroGithub: "https://github.com/hoycid/pagro-admin-app",
-  muzic: "https://muzic-app-bice.vercel.app/",
+  muzic: "https://muzicsoundstudio.netlify.app/",
   muzicGithub: "https://github.com/hoycid/muzic-app",
   er: "https://er-character-planner-teal.vercel.app/",
   erGithub: "https://github.com/hoycid/er-character-planner",
   fashion: "https://souls.fashion/eldenring",
-  fashionGithub: "https://github.com/hoycid/souls-fashion/tree/main"
+  fashionGithub: "https://github.com/hoycid/souls-fashion/tree/main",
 };
 
 export default links;
