c8870aee5d594b03d41a0196abef13efdc3087bf
hoycid <quilangcidrex@gmail.com>
2024-11-02 14:09:55 +1300

    moved content object to external file

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Navigation from "./components/Navbar/Navigation";
 import ProjectPanel from "./components/ProjectPanel/ProjectPanel";
 
 import PORTFOLIO_LIST from "./resources/PORTFOLIO_LIST";
+import content from "./resources/CONTENTOBJ";
 import { socials } from "./resources/socials";
 import { icons } from "./resources/images";
 import { email } from "./resources/socials";
@@ -88,33 +89,6 @@ function App() {
       });
     };
   }, []);
-
-  const content = {
-    greeting: {
-      salut: "Hello there!",
-      intro: "I'm Cid",
-      first:
-        "I'm a dedicated web developer with a passion for creating modern and functional web applications.",
-      second:
-        "Currently, I am focused on developing a strong full-stack skill set to enhance my creative capabilities.",
-      third:
-        "Since 2023, I have lived in Auckland, New Zealand, where I moved to pursue a postgraduate degree in Information Technology.",
-    },
-    about: {
-      tag: "about",
-      title: "Creating modern and functional web applications",
-      subtitle:
-        "I am primarily skilled in the MERN stack, but I also have experience with other web development frameworks like Angular and AdonisJs. Additionally, I've worked with CMS platforms such as WordPress and Shopify, as well as relational databases like MySQL and SQLite. As a digital native, I can quickly gather and learn information online and recently, I've been utilizing AI to achieve the same purpose in an even faster pace.",
-    },
-    portfolio: {
-      tag: "portfolio",
-    },
-    contact: {
-      tag: "contact",
-      title: "Let's get in touch!",
-      subtitle: "I’m open to any interesting opportunity, let’s get in touch!",
-    },
-  };
 
   const knownTechIcons = Object.entries(icons)
     .filter(
