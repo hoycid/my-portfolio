86ba944e0d7b41a378d9e6ccec058690c5517281
hoycid <quilangcidrex@gmail.com>
2026-02-06 11:42:10 +0400

    updated content

--- src/resources/CONTENTOBJ.js
+++ src/resources/CONTENTOBJ.js
@@ -5,17 +5,17 @@ const CONTENTOBJ = {
     salut: "Hello there!",
     intro: "I'm Cid",
     first:
-      "Web development background with an emphasis on front-end development using modern web frameworks and libaries.",
+      "Background in web development, with hands-on experience in front-end work using modern frameworks and libraries.",
     second:
-      "Skilled in building low-code applications using Power Apps and PowerAutomate.",
+      "Experience building low-code solutions with Microsoft Power Apps and Power Automate to streamline workflows and support business processes.",
     third:
-      "Skilled in troubleshooting hardware, software, and network issues to ensure system reliability.",
+      "Experience in handling day-to-day technical support, resolving hardware, software, and network issues to support users and business operations.",
   },
   about: {
     tag: "about",
     title: "Creating modern web applications and business solutions",
     subtitle:
-      "I build practical digital solutions by combining web app development, process automation, and technical support. My toolkit includes web technologies such as React and related frameworks like Next.js. I work with both non-relational and relational databases, including MongoDB, MySQL, and SQLite. Alongside custom development, I design low-code solutions using Microsoft Power Apps and Power Automate to automate workflows, streamline internal processes, and deliver efficient business tools. I also troubleshoot hardware, software, and network issues, helping maintain reliable systems and ensuring technology supports day-to-day operations effectively.",
+      "I build practical digital solutions by combining web app development, process automation, and technical support. My toolkit includes web technologies such as React and related frameworks like Next.js. I work with both non-relational and relational databases, including MongoDB, MySQL, and SQLite. Alongside web development, I design low-code solutions using Microsoft Power Apps and Power Automate to automate workflows, streamline internal processes, and deliver efficient business tools. I also troubleshoot hardware, software, and network issues, helping maintain reliable systems and ensuring technology supports day-to-day operations effectively.",
   },
   portfolio: {
     tag: "portfolio",
